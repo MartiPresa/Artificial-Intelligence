7af16d3259e481d0b1a24fad19d0e488ad716f3f
lunalauti <lauluna817@gmail.com>
2023-09-04 13:08:53 -0300

    funciones de pertenencia de Numerico y Concepto

--- Mamdani.py
+++ Mamdani.py
@@ -1,11 +1,40 @@
-# Num [0,100]-->Concepto [B-,B,B+]
+"""
+Ejercicio: Sistema de puntuación 
+Concepto: B-, B, B+
+Numerico: [0-100]
+Total: [0-100]
+"""
 import numpy as np
 import skfuzzy as fuzz
 import matplotlib.pyplot as plt
 
-# Generate universe variables
-#   * Quality and service on subjective ranges [0, 10]
-#   * Tip has a range of [0, 25] in units of percentage points
-x_qual = np.arange(0, 11, 1)
-x_serv = np.arange(0, 11, 1)
-x_tip  = np.arange(0, 26, 1)
+concept = np.arange(0,11,1)
+numeric = np.arange(0,100,1)
+total = np.arange(0,100,1)
+
+#print(concept)
+
+conceptBmin = fuzz.trimf(concept, [0,0,7])
+conceptBmed = fuzz.trimf(concept, [5, 8,10])
+conceptBmax = fuzz.trapmf(concept, [7,9,10,10])
+
+numericBmin = fuzz.trimf(numeric, [0,0,50])
+numericBmed = fuzz.trimf(numeric, [30, 60,80])
+numericBmax = fuzz.trimf(numeric, [60,100,100]) 
+#numericBmax = fuzz.trapmf(numeric, [70,80,100,100]) 
+
+fig, (ax0, ax1) = plt.subplots(nrows=2, figsize=(8, 9))
+
+ax0.plot(concept, conceptBmin, 'b', linewidth=1.5, label='Bien-')
+ax0.plot(concept, conceptBmed, 'g', linewidth=1.5, label='Bien')
+ax0.plot(concept, conceptBmax, 'r', linewidth=1.5, label='Bien+')
+ax0.set_title('Concept')
+ax0.legend()
+
+ax1.plot(numeric, numericBmin, 'b', linewidth=1.5, label='Baja')
+ax1.plot(numeric, numericBmed, 'g', linewidth=1.5, label='Media')
+ax1.plot(numeric, numericBmax, 'r', linewidth=1.5, label='Alta')
+ax1.set_title('Numeric')
+ax1.legend()
+
+plt.show()
