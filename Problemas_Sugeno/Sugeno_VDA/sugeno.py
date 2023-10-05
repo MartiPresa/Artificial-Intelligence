f1ec6b8fbc4d9a1453a5f067930e01f77f7e4d99
valentina reale <valenreale123@gmail.com>
2023-10-05 20:52:27 -0300

    gen fis con 1 ed

--- Problemas_Sugeno/Sugeno_VDA/sugeno.py
+++ Problemas_Sugeno/Sugeno_VDA/sugeno.py
@@ -83,8 +83,161 @@ data_y = training_data[:,1]
 
 # hago clustering 
 r, c = cl.clustering_sustractivo(training_data,1)
-
+# grafico clusters
 plt.figure()
 plt.scatter(training_data[:,0],training_data[:,1], c=r)
 plt.scatter(c[:,0],c[:,1], marker='X')
 plt.show()
+
+# ------------------------------------------------------GEN FIS------------------------------------------------------
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 17:56:16 2020
+
+@author: Daniel Albornoz
+
+Implementación similar a genfis2 de Matlab.
+Sugeno type FIS. Generado a partir de clustering substractivo.
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import MinMaxScaler
+import time
+
+def gaussmf(data, mean, sigma):
+    return np.exp(-((data - mean)**2.) / (2 * sigma**2.))
+
+class fisRule:
+    def __init__(self, centroid, sigma):
+        self.centroid = centroid
+        self.sigma = sigma
+
+class fisInput:
+    def __init__(self, min,max, centroids):
+        self.minValue = min
+        self.maxValue = max
+        self.centroids = centroids
+
+
+    def view(self):
+        x = np.linspace(self.minValue,self.maxValue,20)
+        plt.figure()
+        for m in self.centroids:
+            s = (self.minValue-self.maxValue)/8**0.5
+            y = gaussmf(x,m,s)
+            plt.plot(x,y)
+
+class fis:
+    def __init__(self):
+        self.rules=[]
+        self.memberfunc = []
+        self.inputs = []
+
+
+    def genfis(self, data, radii):
+
+        start_time = time.time()
+        labels, cluster_center = cl.clustering_sustractivo(data, radii)
+
+        print("--- %s seconds ---" % (time.time() - start_time))
+        n_clusters = len(cluster_center)
+
+        cluster_center = cluster_center[:,:-1]
+        P = data[:,:-1]
+        #T = data[:,-1]
+        maxValue = np.max(P, axis=0)
+        minValue = np.min(P, axis=0)
+
+        self.inputs = [fisInput(maxValue[i], minValue[i],cluster_center[:,i]) for i in range(len(maxValue))]
+        self.rules = cluster_center
+        self.entrenar(data)
+
+    def entrenar(self, data):
+        P = data[:,:-1]
+        T = data[:,-1]
+        #___________________________________________
+        # MINIMOS CUADRADOS (lineal)
+        sigma = np.array([(i.maxValue-i.minValue)/np.sqrt(8) for i in self.inputs])
+        f = [np.prod(gaussmf(P,cluster,sigma),axis=1) for cluster in self.rules]
+
+        nivel_acti = np.array(f).T
+        print("nivel acti")
+        print(nivel_acti)
+        sumMu = np.vstack(np.sum(nivel_acti,axis=1))
+        print("sumMu")
+        print(sumMu)
+        P = np.c_[P, np.ones(len(P))]
+        n_vars = P.shape[1]
+
+        orden = np.tile(np.arange(0,n_vars), len(self.rules))
+        acti = np.tile(nivel_acti,[1,n_vars])
+        inp = P[:, orden]
+
+
+        A = acti*inp/sumMu
+
+        # A = np.zeros((N, 2*n_clusters))
+        # for jdx in range(n_clusters):
+        #     for kdx in range(nVar):
+        #         A[:, jdx+kdx] = nivel_acti[:,jdx]*P[:,kdx]/sumMu
+        #         A[:, jdx+kdx+1] = nivel_acti[:,jdx]/sumMu
+
+        b = T
+
+        solutions, residuals, rank, s = np.linalg.lstsq(A,b,rcond=None)
+        self.solutions = solutions #.reshape(n_clusters,n_vars)
+        print(solutions)
+        return 0
+
+    def evalfis(self, data):
+        sigma = np.array([(input.maxValue-input.minValue) for input in self.inputs])/np.sqrt(8)
+        f = [np.prod(gaussmf(data,cluster,sigma),axis=1) for cluster in self.rules]
+        nivel_acti = np.array(f).T
+        sumMu = np.vstack(np.sum(nivel_acti,axis=1))
+
+        P = np.c_[data, np.ones(len(data))]
+
+        n_vars = P.shape[1]
+        n_clusters = len(self.rules)
+
+        orden = np.tile(np.arange(0,n_vars), n_clusters)
+        acti = np.tile(nivel_acti,[1,n_vars])
+        inp = P[:, orden]
+        coef = self.solutions
+
+        return np.sum(acti*inp*coef/sumMu,axis=1)
+
+
+    def viewInputs(self):
+        for input in self.inputs:
+            input.view()
+
+
+
+# ------------------------------------------------------TEST GEN FIS 1 ed------------------------------------------------------
+def my_exponential(A, B, C, x):
+    return A*np.exp(-B*x)+C
+
+# data_x = np.arange(-10,10,0.1)
+# data_y = -0.5*data_x**3-0.6*data_x**2+10*data_x+1 #my_exponential(9, 0.5,1, data_x)
+
+plt.plot(data_x, data_y)
+# plt.ylim(-20,20)
+# plt.xlim(-7,7)
+
+data = np.vstack((data_x, data_y)).T
+
+fis2 = fis()
+fis2.genfis(data, 1.1)
+fis2.viewInputs()
+r = fis2.evalfis(np.vstack(data_x))
+
+plt.figure()
+plt.plot(data_x,data_y)
+plt.plot(data_x,r,linestyle='--')
+plt.show()
+fis2.solutions
+fis2.rules
