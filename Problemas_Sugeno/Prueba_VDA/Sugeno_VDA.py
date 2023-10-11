8af48fb736e7aa56a6dea7838d030a89ce1f77b5
MartiPresa <102812269+MartiPresa@users.noreply.github.com>
2023-10-10 21:54:22 -0300

    Version Final

---
+++ Problemas_Sugeno/Prueba_VDA/Sugeno_VDA.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from sklearn.preprocessing import MinMaxScaler
+from scipy.spatial import distance_matrix
+import time
+from sklearn.model_selection import train_test_split
+from random import choice
+import pandas as pandas
+from sklearn.metrics import mean_squared_error
+from Sugeno_generico import fis_Sugeno
+import clustering as cl
+import copy
+
+def lee_arch(path):
+    datos = []
+    with open(path, "r") as file:
+        valorx = 0
+        for linea in file:
+            valory = float(linea.strip())
+            datos.append([valorx, valory])
+            valorx = valorx+2.5
+    auxDatos = np.array(datos)
+    return auxDatos
+
+def split_data(data):
+    X_train, X_test, y_train, y_test = train_test_split(data[:,0], data[:,1], test_size=0.2, random_state=42)
+    training_data =  np.column_stack((X_train, y_train))
+    test_data =  np.column_stack((X_test,y_test))
+    return training_data,test_data
+
+def graficar(auxDatos):
+
+    valores_x = auxDatos[:, 0]  
+    valores_y = auxDatos[:, 1] 
+
+    plt.scatter(valores_x, valores_y, label = "Datos") 
+    plt.xlabel('Tiempo [ms]')
+    plt.ylabel('VDA')
+    plt.title('Gráfico de datos')
+    plt.legend() 
+    plt.grid(True) 
+    plt.show()  #
+
+def sobremuestreo(data):
+   
+    num_filas, num_columnas = data.shape
+    promedios = np.zeros((num_filas-1, num_columnas))
+    for i in range(num_filas-1):
+        promedios [i,:] = (abs(data[i,:] + data[i+1,:])/2.0)
+
+    return promedios
+
+
+path = 'C:/Users/marti/OneDrive/Documentos/GitHub/Artificial-Intelligence/Problemas_Sugeno/Sugeno_VDA/samplesVDA1.txt'
+datos = lee_arch(path)
+data = []
+training_data = []
+test_data = [] 
+
+data = lee_arch(path)
+training_data, test_data = split_data(data)
+data_x = training_data[:,0] 
+data_y = training_data[:,1] # target
+
+graficar(data)
+
+#------------------------------Modelos con clustering Kmeans
+mse = []
+test_mse = 10000
+training_mse = 0
+k_clusters = []
+modelos = []
+k = 0
+i = 3
+while (i <= 20):
+    sugeno = fis_Sugeno()
+    modelos.append(sugeno)
+    sugeno.genfis_k(training_data,i) # ra,rb
+    reg = sugeno.evalModelo(np.vstack(data_x))
+    mse_train,mse_test = sugeno.mspe(training_data,test_data) # retorna mse_train,mse_test,mspe_train,mspe_test
+    mse.append(mse_test)
+    if(test_mse > mse_test): # guardo el mejor modelo
+        test_mse = mse_test
+        training_mse = mse_train
+        k = i
+        mejor_sugeno = copy.deepcopy(sugeno)
+    k_clusters.append(i)
+    i += 1
+
+
+plt.plot(k_clusters, mse, label='Datos', marker='o', linestyle='-')  # Graficar una línea con puntos
+plt.ylabel('mspe')
+plt.xlabel('Cantidad de reglas R')
+plt.title('R vs mspe')
+
+# Establecer el rango en el eje Y para mostrar todos los datos sin superponerse
+plt.ylim(min(mse) - 0.05, max(mse) + 0.05)
+
+plt.xticks(k_clusters)
+plt.legend()  # Mostrar la leyenda
+plt.grid(True)  # Mostrar cuadrícula
+plt.show()  # Mostrar el gráfico
+
+
+mejor_sugeno.muestra(training_data,test_data,f"Sugeno con {k} clusters")
+mejor_sugeno.grafica_mse(test_data)
+
+#------------------Entreno Sugeno con todos los datos y con los hiperparametros del mejor_sugeno
+nuevo_sugeno = fis_Sugeno()
+nuevo_sugeno.genfis_k(data,k)
+nuevo_training_mse,mse_test = nuevo_sugeno.mse(data,data)
+
+datos_sobremuestreo = sobremuestreo(data)
+mejor_sugeno.grafica_mse(datos_sobremuestreo)
+
+nuevo_mse,mse_test = nuevo_sugeno.mse(datos_sobremuestreo,datos_sobremuestreo)
+nuevo_sugeno.grafica_mse(datos_sobremuestreo)
+
+
--- Problemas_Sugeno/Prueba_VDA/Sugeno_generico.py
+++ Problemas_Sugeno/Prueba_VDA/Sugeno_generico.py
@@ -9,7 +9,7 @@ from skfuzzy import control as ctrl
 from random import choice
 import pandas as pandas
 from sklearn.metrics import mean_squared_error
-import clusteringSustractivo as cl
+import clustering as cl
 from sklearn.model_selection import train_test_split
 
 # Calcula el nivel de pertenencia de cada dato a las gaussianas
@@ -62,112 +62,62 @@ class fis_Sugeno:
         return self.mse_train
     def get_mse_test(self):
         return self.mse_test
-    
-    # def clusteringSustractivo(datos, r):
-    #     return cl.clustering_sustractivo(datos,r)
 
     def genfis_k(self, data, k):
         start_time = time.time()
         self.k = k
-        # self.labels, self.clusters = cl.clustering_sustractivo(data, radii,rb) # hace clustering
         self.labels, self.clusters = cl.clustering_kmeans(data,k) # hace clustering
         cluster_center = self.clusters
-        # print(f'CLUSTERS = {self.clusters}')
-        #print("--- %s seconds ---" % (time.time() - start_time))
-        n_clusters = len(cluster_center)    #numero de clusters
+        n_clusters = len(cluster_center)
 
-        cluster_center = cluster_center[:,:-1] #se queda con todas las columnas menos la ultima
-        P = data[:,:-1] # se queda con la primera columna, osea los X
-        # T = data[:,-1]
+        cluster_center = cluster_center[:,:-1] 
+        P = data[:,:-1] 
         maxValue = np.max(P, axis=0)
         minValue = np.min(P, axis=0)
 
         self.inputs = [fisInput(maxValue[i], minValue[i],cluster_center[:,i]) for i in range(len(maxValue))]
-        #print(f'INPUTS = {self.inputs}')
         self.rules = cluster_center
         self.training(data)
 
     def genfis(self, data, radii,rb):
 
         start_time = time.time()
-        # self.labels, self.clusters = cl.clustering_sustractivo(data, radii,rb) # hace clustering
-        self.labels, self.clusters = cl.clustering_kmeans(data, radii,rb) # hace clustering
+        self.labels, self.clusters = cl.clustering_kmeans(data, radii,rb) 
         cluster_center = self.clusters
-        # print(f'CLUSTERS = {self.clusters}')
-        #print("--- %s seconds ---" % (time.time() - start_time))
         n_clusters = len(cluster_center)    #numero de clusters
 
-        cluster_center = cluster_center[:,:-1] #se queda con todas las columnas menos la ultima
-        P = data[:,:-1] # se queda con la primera columna, osea los X
-        # T = data[:,-1]
+        cluster_center = cluster_center[:,:-1] 
+        P = data[:,:-1] 
         maxValue = np.max(P, axis=0)
         minValue = np.min(P, axis=0)
 
         self.inputs = [fisInput(maxValue[i], minValue[i],cluster_center[:,i]) for i in range(len(maxValue))]
-        #print(f'INPUTS = {self.inputs}')
         self.rules = cluster_center
         self.training(data)
 
     def training(self, data):
-        datos_entrada = data[:,:-1] #elimina la ultima columna
-        target = data[:,-1] #se queda con la ultima columna
-        #___________________________________________
-        # MINIMOS CUADRADOS (lineal)
-        # Calcula los sigmas de cada campana de gauss
+        datos_entrada = data[:,:-1] 
+        target = data[:,-1] 
         sigma = np.array([(i.maxValue-i.minValue)/np.sqrt(8) for i in self.inputs])
 
-        #F contiene un array por cada cluster, que tienen los arrays? ni puta idea
         f = [np.prod(gaussmf(datos_entrada,cluster,sigma),axis=1) for cluster in self.rules]
-        # print(f'valor de F {f}')
+     
+        nivel_acti = np.array(f).T 
+        sumMu = np.vstack(np.sum(nivel_acti,axis=1))
 
-        nivel_acti = np.array(f).T #pasa el valor de f traspuesto
-        #quedan los valores de pertenencia a los clusters por columnas
-        # print("nivel acti")
-        # print(nivel_acti)
-        sumMu = np.vstack(np.sum(nivel_acti,axis=1))# por cada dato suma el valor de pertenencia a cada cluster y lo pone en un vector
-        # print("sumMu")
-        # print(sumMu)
-
-        datos_entrada = np.c_[datos_entrada, np.ones(len(datos_entrada))] #Agrega una columna de 1's a la matriz de datos de entrada
-        # print(datos_entrada)
-        n_vars = datos_entrada.shape[1] #almacena la cantidad de columnas en la matriz anterior
-        #print(f'SHAPE DATOS {datos_entrada.shape}')
-
-        #n_vars = 2
-        # np.arange(0,n_vars) = [0,1]
-        #tile repite lo que haya en el primer parametro, la cantidad de veces indicada en
-        #el segundo parametro. En este caso escribe [0 1 0 1 0 1]
+        datos_entrada = np.c_[datos_entrada, np.ones(len(datos_entrada))] 
+        n_vars = datos_entrada.shape[1] 
         orden = np.tile(np.arange(0,n_vars), len(self.rules)) 
-        
-        #En este caso, como el segundo parametro es [1,n_vars], devuelve un array que contiene
-        #un array con la cantidad de elementos repetidos n_vars veces. 
+      
         acti = np.tile(nivel_acti,[1,n_vars]) 
         inp = datos_entrada[:, orden]
-        #Estos ultimos 2 pasos amplian las matrices a 6 columnas
-        # print(f'acti {acti}')
-        # print(f'inp{inp}')
-
-        #Hace una especie de agregacion entre todas las reglas (Suavizar rectas)
-        A = acti*inp/sumMu # hace una especie de promedio pesado CREO
-        #acti: valor de pertenencia
-        #inp: valor de la mf en un valor x 
-        #sumMu: suma de los valores de pertenencia
-        
        
-
-        # print(f'VALOR DE A {A}')
-        # A = np.zeros((N, 2*n_clusters))
-        # for jdx in range(n_clusters):
-        #     for kdx in range(nVar):
-        #         A[:, jdx+kdx] = nivel_acti[:,jdx]*P[:,kdx]/sumMu
-        #         A[:, jdx+kdx+1] = nivel_acti[:,jdx]/sumMu
-
+        A = acti*inp/sumMu 
         b = target
 
         #CUADRADOS MINIMOS --> lstsq 
-        solutions, residuals, rank, s = np.linalg.lstsq(A,b,rcond=None) #solutions es el mse CREO
-        self.solutions = solutions #.reshape(n_clusters,n_vars)
-        # print(solutions)
+        solutions, residuals, rank, s = np.linalg.lstsq(A,b,rcond=None) 
+        self.solutions = solutions 
         return 0
 
     def evalModelo(self, data):
@@ -207,20 +157,24 @@ class fis_Sugeno:
         salida_train = self.evalModelo(np.vstack(training_data[:,0]))
         mse_train = mean_squared_error(target_train, salida_train)
         mse_test = mean_squared_error(target_test, salida_test)
-        mspe_test = mse_test * 100 / np.mean(target_test)
-        mspe_train = mse_train * 100 / np.mean(target_train)
-        return mspe_train,mspe_test
+        # mspe_test = mse_test * 100 / np.mean(target_test)
+        # mspe_train = mse_train * 100 / np.mean(target_train)
+        return self.error_porcentual(mse_train,training_data),self.error_porcentual(mse_test,test_data)
     
-    def grafica_mse(self, datos,mse_train):
+    def error_porcentual(self,mse,data):
+        target_test = data[:,-1]
+        return mse * 100 / np.mean(target_test)
+    
+    def grafica_mse(self, datos):
             salidas = self.evalModelo(np.vstack(datos[:,0]))
-            error = mean_squared_error(datos[:, 1], salidas)
+            error_test= mean_squared_error(datos[:, 1], salidas)
+            error_test = self.error_porcentual(error_test,datos)
+            error_test = "{:.2f}".format(error_test)
+            # mspe_train = "{:.2f}".format(mspe_train)
             plt.figure(figsize=(10,5))
             plt.scatter(datos[:, 0], datos[:, 1], label='Targets', c='g')
             plt.scatter(datos[:, 0], salidas,
-                        label='Salida del modelo', c='red')
-
-            # Grafica las lineas entre el target y la salida del modelo
-            # Si se equivocan con mas del 20% lo marca en rojo
+                        label='Salida del modelo (Y)', c='red')
             for i, point in enumerate(datos[:, 0]):
                 x = datos[i, 0]
                 y = datos[i, 1]
@@ -231,21 +185,18 @@ class fis_Sugeno:
             plt.xlabel('Tiempo [ms]')
             plt.ylabel('VDA')
             plt.title('Targets vs Salida del modelo')
-            plt.text(200, 620, f'MSE Test= {error}', bbox={
+            plt.text(200, 620, f'MSE Test= {error_test}%', bbox={
                     'facecolor': 'skyblue', 'alpha': 0.5, 'pad': 8})
-            plt.text(200, 580, f'MSE Train= {mse_train}', bbox={
-                    'facecolor': 'skyblue', 'alpha': 0.5, 'pad': 8})
+            # plt.text(200, 580, f'MSE Train= {mspe_train}%', bbox={
+            #         'facecolor': 'skyblue', 'alpha': 0.5, 'pad': 8})
             plt.legend()
             plt.grid(True)
             plt.show()
 
-            return error
-
     def muestra(self,training_data,testing_data,title):
         data_x = training_data[:,0] 
-        data_y = training_data[:,1] # target
+        data_y = training_data[:,1] 
         reg = self.evalModelo(np.vstack(data_x))
-        # r,c = cl.clustering_sustractivo(training_data,1)
         r,c = self.labels,self.clusters
         fig,(ax0, ax1) = plt.subplots(nrows=2,figsize=(15, 10))
         ax0.set_title(title)
@@ -253,10 +204,6 @@ class fis_Sugeno:
         ax0.scatter(c[:,0],c[:,1], marker='X')
         self.viewInputs(ax1)
         ax1.set_title('Campanas de Gauss')
-        # ax2.set_title('Rectas?')
-        # ax2.plot(data_x,data_y)
-        # ax2.plot(data_x,reg,linestyle='--')
-        # ax2.scatter(testing_data[:,0],testing_data[:,1], c='green') # muestro los datos de test para ver cuanto error hay con el modelo
         ax0.scatter(c[:,0],c[:,1], marker='X')
         plt.show()
 
@@ -268,16 +215,11 @@ class fis_Sugeno:
 
 def lee_arch(path):
     datos = []
-    # Abre el archivo y lee los datos
     with open(path, "r") as file:
         valorx = 0
         for linea in file:
-            # Convierte los valores de cadena a números de punto flotante
             valory = float(linea.strip())
-            # Agrega los valores a la lista de datos
             datos.append([valorx, valory])
             valorx = valorx+2.5
-
-    # Convierte la lista de datos en una matriz NumPy
     auxDatos = np.array(datos)
     return auxDatos
